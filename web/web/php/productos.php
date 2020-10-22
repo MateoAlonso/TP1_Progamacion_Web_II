c5b660ba95e04860b48962383d659dc5112cc407
MateoAlonso <mateoalonsogioia27@gmail.com>
2020-10-21 22:01:06 -0300

    arreglue problema con el index de detalles

--- web/web/php/productos.php
+++ web/web/php/productos.php
@@ -14,7 +14,7 @@
             
         ],
         2=>[
-            "id_producto"=>1,
+            "id_producto"=>2,
             "nombre"=>"rolex two",
             "marca"=>"rolex",
             "modelo"=>"vintage",
@@ -25,7 +25,7 @@
             "precio"=>15000
         ],
         3=>[
-            "id_producto"=>1,
+            "id_producto"=>3,
             "nombre"=>"rolex wind",
             "marca"=>"rolex",
             "modelo"=>"vintage",
@@ -36,7 +36,7 @@
             "precio"=>14000
         ],
         4=>[
-            "id_producto"=>1,
+            "id_producto"=>4,
             "nombre"=>"rolex tiger",
             "marca"=>"rolex",
             "modelo"=>"vintage",
@@ -47,7 +47,7 @@
             "precio"=>5000
         ],
         5=>[
-            "id_producto"=>1,
+            "id_producto"=>5,
             "nombre"=>"rolex near",
             "marca"=>"rolex",
             "modelo"=>"vintage",
@@ -58,7 +58,7 @@
             "precio"=>32000
         ],
         6=>[
-            "id_producto"=>1,
+            "id_producto"=>6,
             "nombre"=>"rolex tree",
             "marca"=>"rolex",
             "modelo"=>"vintage",
@@ -69,7 +69,7 @@
             "precio"=>3000
         ],
         7=>[
-            "id_producto"=>1,
+            "id_producto"=>7,
             "nombre"=>"rolex brown",
             "marca"=>"rolex",
             "modelo"=>"vintage",
@@ -80,7 +80,7 @@
             "precio"=>4000
         ],
         8=>[
-            "id_producto"=>1,
+            "id_producto"=>8,
             "nombre"=>"rolex rubi",
             "marca"=>"rolex",
             "modelo"=>"vintage",
@@ -91,7 +91,7 @@
             "precio"=>3000
         ],
         9=>[
-            "id_producto"=>1,
+            "id_producto"=>9,
             "nombre"=>"rolex perl",
             "marca"=>"rolex",
             "modelo"=>"vintage",
@@ -102,7 +102,7 @@
             "precio"=>12000
         ],
         10=>[
-            "id_producto"=>1,
+            "id_producto"=>10,
             "nombre"=>"rolex shine",
             "marca"=>"rolex",
             "modelo"=>"vintage",
@@ -113,7 +113,7 @@
             "precio"=>110000
         ],
         11=>[
-            "id_producto"=>1,
+            "id_producto"=>11,
             "nombre"=>"rolex yux",
             "marca"=>"rolex",
             "modelo"=>"vintage",
@@ -124,7 +124,7 @@
             "precio"=>2000
         ],
         12=>[
-            "id_producto"=>1,
+            "id_producto"=>12,
             "nombre"=>"rolex ten",
             "marca"=>"rolex",
             "modelo"=>"vintage",
@@ -135,7 +135,7 @@
             "precio"=>14000
         ],
         13=>[
-            "id_producto"=>1,
+            "id_producto"=>13,
             "nombre"=>"rolex twister",
             "marca"=>"rolex",
             "modelo"=>"vintage",
@@ -146,7 +146,7 @@
             "precio"=>3300
         ],
         14=>[
-            "id_producto"=>1,
+            "id_producto"=>14,
             "nombre"=>"rolex rally",
             "marca"=>"rolex",
             "modelo"=>"vintage",
@@ -157,7 +157,7 @@
             "precio"=>2000
         ],
         15=>[
-            "id_producto"=>1,
+            "id_producto"=>15,
             "nombre"=>"rolex front",
             "marca"=>"rolex",
             "modelo"=>"vintage",
@@ -168,7 +168,7 @@
             "precio"=>4000
         ],
         16=>[
-            "id_producto"=>1,
+            "id_producto"=>16,
             "nombre"=>"rolex gear",
             "marca"=>"rolex",
             "modelo"=>"vintage",
@@ -179,7 +179,7 @@
             "precio"=>7000
         ],
         17=>[
-            "id_producto"=>1,
+            "id_producto"=>17,
             "nombre"=>"rolex chaarm",
             "marca"=>"rolex",
             "modelo"=>"vintage",
@@ -190,7 +190,7 @@
             "precio"=>5000
         ],
         18=>[
-            "id_producto"=>1,
+            "id_producto"=>18,
             "nombre"=>"rolex fiurex",
             "marca"=>"rolex",
             "modelo"=>"vintage",
@@ -201,7 +201,7 @@
             "precio"=>4000
         ],
         19=>[
-            "id_producto"=>1,
+            "id_producto"=>19,
             "nombre"=>"rolex fedex",
             "marca"=>"rolex",
             "modelo"=>"vintage",
@@ -212,7 +212,7 @@
             "precio"=>2000
         ],
         20=>[
-            "id_producto"=>1,
+            "id_producto"=>20,
             "nombre"=>"rolex claim",
             "marca"=>"rolex",
             "modelo"=>"vintage",
