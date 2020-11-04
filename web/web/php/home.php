ae7395588e2af69ef624efaca054353744302530
emaaviz <emanuel.vizcarra@davinci.edu.ar>
2020-11-04 11:07:14 -0300

    separe header, footer del home con el "require"

--- web/web/php/home.php
+++ web/web/php/home.php
@@ -1,53 +1,4 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Trabajo WEB II</title>
-    <link rel="stylesheet" href="css/bootstrap.css">
-    <link rel="stylesheet" href="css/style.css">
-    <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
-</head>
-<body id="fp" class="margenftfb fpi barrahorizontalno">
-    <div class="container-fluid">
-        
-        <!-- Acá estoy parado en index.php, no en home.php, por eso hay que linkear las cosas como si estuvieramos en index.php -->
-            
-        <!-- H1 oculto -->
 
-        <h1 class="d-none">Trabajo WEB II</h1>
-            
-        <nav class="navbar navbar-expand-md bg-dark navbar-dark fixed-top">
-
-            <!-- Logo -->
-    
-            <a href="index.php" class="navbar-brand ml-2">
-                <img src="img/rolex-40x40.jpeg" alt="placeholder logo" width="40" height="40" class="img-fluid rounded-circle">
-            </a>
-    
-            <!-- Hamburquesa -->
-    
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#hamb">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-    
-            <!-- Links -->
-            <div class="collapse navbar-collapse" id="hamb">
-                <ul class="navbar-nav ml-auto mr-2">
-                    <li class="nav-item">
-                        <a href="index.php" class="nav-link active">Inicio</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="php/contacto.php" class="nav-link">Contacto</a>
-                    </li>
-                </ul>
-            </div>
-        </nav>
-
-        <header class="container">
-            <h2 class="h1 text-light text-center my-5 fuenteb">Productos: Nuestros relojes</h2>
-        </header>
         
         <section class="row justify-content-center">
 
@@ -498,33 +449,9 @@
             </div>
         </section>
         
-        <!-- Footer  -->
-
-        <section class="containter-fluid bg-dark fixed-bottom">
-            <div class="row">
-                <div class="card-footer text-muted col-12">
-                    <h4 class= "h6 text-center mt-1 ml-3">Alonso, Mateo | Emanuel Vizcarra | Grupo 4 | Programación Web II | 2do Cuatrimestre 2020</h4>
-                </div>
-            </div>
-        </section>
+  
             
     </div>
-            
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-
-
 
     <script src="js/jquery-3.4.1.min.js"></script>
     <script src="js/bootstrap.bundle.min.js"></script>   
