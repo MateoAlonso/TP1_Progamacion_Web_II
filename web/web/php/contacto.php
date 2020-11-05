f011f4a433614ec1f61647dee1aecb4104ad73eb
emaaviz <emanuel.vizcarra@davinci.edu.ar>
2020-11-05 08:01:51 -0300

    retrocedi una cosa que cambie por error

--- web/web/php/contacto.php
+++ web/web/php/contacto.php
@@ -1,3 +1,47 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Contacto</title>
+    <link rel="stylesheet" href="../css/bootstrap.css">
+    <link rel="stylesheet" href="../css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+</head>
+<body class="fondo margenftfb">
+    <div class="container-fluid">
+
+        <!-- H1 oculto -->
+
+        <h1 class="d-none">Pepsi Music</h1>
+            
+        <nav class="navbar navbar-expand-md bg-dark navbar-dark fixed-top">
+
+            <!-- Logo -->
+    
+            <a href="../index.php" class="navbar-brand ml-2">
+                <img src="../img/rolex-40x40.jpeg" alt="pepsi logo" width="40" height="40" class="img-fluid rounded-circle">
+            </a>
+    
+            <!-- Hamburquesa -->
+    
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#hamb">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+    
+            <!-- Links -->
+            <div class="collapse navbar-collapse" id="hamb">
+                <ul class="navbar-nav ml-auto mr-2">
+                    <li class="nav-item">
+                        <a href="../index.php" class="nav-link">Inicio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="contacto.php" class="nav-link active">Contacto</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
 
         <!-- Formulario -->
 
